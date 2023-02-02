126dbebfca510ab2a724603530cf36b54ee62161
rioxraman <ramanpkgupta@gmail.com>
2023-02-02 11:47:10 +0530

    mongoy

---
+++ config/mongoose.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+
+ mongoose.connect('mongodb://127.0.0.1:27017/contact_list')
+  .then(() => console.log('MongoDB Connected!'));
+  
+  const db =mongoose.connection
+
+  db.on('open',()=>{console.log("success");})
--- index.js
+++ index.js
@@ -2,21 +2,22 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const port = 3000
-
-var contactList = [
-  {
-      name: "Rio",
-      phone: "1111111111",
-  },
-  {
-      name: "Tony Stark",
-      phone: "1234567890"
-  },
-  {
-      name: "Richard",
-      phone: "12131321321"
-  }
-]
+const db = require('./config/mongoose');
+const Contact =require('./models/contact')
+// var contactList = [
+//   {
+//       name: "Rio",
+//       phone: "1111111111",
+//   },
+//   {
+//       name: "Tony Stark",
+//       phone: "1234567890"
+//   },
+//   {
+//       name: "Richard",
+//       phone: "12131321321"
+//   }
+// ]
 
 app.set('view engine','ejs')
 app.set('views',path.join(__dirname, 'views'))
@@ -24,32 +25,60 @@ app.set('views',path.join(__dirname, 'views'))
 app.use(express.urlencoded())
 app.use(express.static('assets'))
 
-app.get('/', (req, res) => {
-  res.render('index',{
-    title:"nodeclass",
-    user:{
-      name: "Raman"
-    } 
+app.get('/dash', (req, res) => {
+  Contact.find({},(err,contacts)=>{
+    if(err){
+      console.log(err);
+    }
+    res.render('project',{
+      title:"nodeclass",
+      name:contacts
+    })
+  })
+
+  // res.render('index',{
+  //   title:"nodeclass",
+  //   user:{
+  //     name: "Raman"
+  //   } 
+  // })
+})
+
+app.get('/',(req,res)=>{
+  Contact.find({},(err,contacts)=>{
+    res.render("project",{
+      name:contacts
+    })
   })
 })
 
+
 app.post('/contact',(req,res)=>{
-  console.log("expected error :",req.body);
-  contactList.push(req.body)
-  return res.redirect('/viewlist')
+  Contact.create({
+    name:req.body.name,phone:req.body.phone
+  }, function (err,newContact) {
+    if(err){
+      console.log(err);
+    }
+    console.log(newContact);
+    return res.redirect('back')
+  })
+  // console.log("expected error :",req.body);
+  // contactList.push(req.body)
+  // return res.redirect('/viewlist')
 })
 // app.post('/contact',function name(params) {
   
 // })
 
-app.get('/viewlist', (req, res) => {
-  res.render('project',{
-    title:"contact",
+// app.get('/viewlist', (req, res) => {
+//   res.render('project',{
+//     title:"contact",
     
-    name:contactList
+//     name:contactList
     
-  })
-})
+//   })
+// })
 
 app.get('/delete', (req, res) => {
   console.log(req.query);
